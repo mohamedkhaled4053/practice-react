dcd1642ffbcfd754237dd71c615513489f57c5a0
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-14 14:45:31 +0200

    9-custom-hooks: remove warnings

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useFetch } from './2-useFetch'
 
 // ATTENTION!!!!!!!!!!
--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,21 +1,18 @@
-import { useState, useEffect } from 'react';
-
-
+import { useState, useEffect } from "react";
 
 export function useFetch(url) {
-    const [loading, setLoading] = useState(true)
-    const [products, setProducts] = useState([])
-  
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
     const getProducts = async () => {
-      const response = await fetch(url)
-      const products = await response.json()
-      setProducts(products)
-      setLoading(false)
-    }
-  
-    useEffect(() => {
-      getProducts()
-    }, [url])
-  
-    return {products, loading}
-  }
+      const response = await fetch(url);
+      const products = await response.json();
+      setProducts(products);
+      setLoading(false);
+    };
+    getProducts();
+  }, [url]);
+
+  return { products, loading };
+}
